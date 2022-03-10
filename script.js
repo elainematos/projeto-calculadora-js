342e3361966ef03e4a50905106423024224f5f52
Elaine matos <nany.sara@hotmail.com>
2022-03-10 01:17:56 -0300

    Criando funções das operações

---
+++ script.js
@@ -0,0 +1,47 @@
+/*let cursor = document.querySelector(".pisca");
+let botao1 = document.querySelector(".btn-1");
+function calculos() {
+    
+
+}
+document.querySelector('.teclado-botao').addEventListener("click", mostrarNumero);
+
+function mostrarNumero() {
+    
+    document.querySelector('.numeros').textContent;
+    //botao1.innerHTML = value;
+};*/
+let piscar = document.querySelector('.pisca')  
+    function clicou(n){
+        let numero = document.querySelector('.numeros');
+        numero.innerHTML = n;
+        numero.classList.remove('pisca');
+       
+    }
+    
+    function soma(numeros1, numeros2) {
+        let resultado = numeros1 + numeros2;
+        
+       
+
+    }
+    //numeros.innerHTML = "";
+    function limpar(){
+        let numeros2 = document.querySelector('.numeros'); 
+        
+        if(numeros2 == numeros2){
+            document.querySelector('.numeros').innerHTML = "";
+            
+           
+        }else {
+            numeros2.classList.add('.pisca');
+            
+        
+        }
+       
+
+    }
+
+    
+    
+  
